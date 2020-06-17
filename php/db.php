b6db47eb226e40949b83e2301bfc6a8f5d92eaba
Rui Duarte <r3v3nant@gmail.com>
2020-06-17 18:33:43 +0100

    Update db.php

--- php/db.php
+++ php/db.php
@@ -5,7 +5,7 @@ $password = "123";
 $dbname = "cs";
 
 // Create connection
-$conn = new PDO( 'mysql:host=localhost;dbname=cs', $username, $password );
+$conn = new PDO( 'mysql:host=' .$servername. ';dbname=' .$dbname, $username, $password );
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
